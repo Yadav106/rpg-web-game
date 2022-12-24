a5854fe615dba335484db6db0ba0493ff7892c9d
Yadav106 <sahil.jp.yadav@gmail.com>
2022-12-24 12:25:49 +0530

    added the restart function

--- script.js
+++ script.js
@@ -227,5 +227,13 @@ function defeatMonster() {
 }
 
 function restart() {
-
+    xp = 0;
+    health = 100;
+    gold = 50;
+    currentWeapon = 0;
+    inventory = ['stick'];
+    goldText.innerText = gold;
+    healthText.innerText = health;
+    xpText.innerText = xp;
+    goTown();
 }
